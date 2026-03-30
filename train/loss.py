7ec5468c23ae5d6eee41499cdb3da351a6aaae56
Kunqi Yan <114635000+Kqboy@users.noreply.github.com>
2026-03-30 20:54:22 +0800

    Update loss.py

--- train/loss.py
+++ train/loss.py
@@ -1,1 +1,12 @@
+import torch
+import torch.nn.functional as F
 
+def similarity_loss(img_hash, txt_hash):
+    return F.mse_loss(img_hash, txt_hash)
+
+def quantization_loss(prob):
+    binary = torch.round(prob)
+    return F.mse_loss(prob, binary)
+
+def total_loss(img_hash, txt_hash, prob, alpha=0.1):
+    return similarity_loss(img_hash, txt_hash) + alpha * quantization_loss(prob)
