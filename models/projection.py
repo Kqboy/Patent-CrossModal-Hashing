4c0c3d09d633673cbc5cdac7ff7adcf33b24cc62
Kunqi Yan <114635000+Kqboy@users.noreply.github.com>
2026-03-30 20:51:45 +0800

    Update projection.py

--- models/projection.py
+++ models/projection.py
@@ -1,1 +1,10 @@
+import torch
+import torch.nn as nn
 
+class Projection(nn.Module):
+    def __init__(self, high_dim=64, low_dim=16):
+        super().__init__()
+        self.proj = nn.Linear(high_dim, low_dim, bias=False)
+
+    def forward(self, x):
+        return self.proj(x)
